87892f0dc9820c29402254b2ec2f9fe9946d8a30
unknown <baris.sahin.can@gmail.com>
2019-08-21 22:40:37 -0400

    deployment test 3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config= require("config");
-const nodemailer=require("nodemailer");
+const path = require('path');
 
 
 mongoose.Promise = global.Promise
@@ -17,6 +17,10 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 // Add routes, both API and view
 app.use(routes);
